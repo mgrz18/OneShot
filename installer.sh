18ca43fe7e81df3c5bd4d602a9815ca2dcb3d263
Miguel García <miguel.gara@outlook.com>
2024-07-06 16:45:27 -0600

    Added installer.sh to fork

---
+++ installer.sh
@@ -0,0 +1,11 @@
+# This script was extracted from: https://web.archive.org/web/20220424020919/https://raw.githubusercontent.com/drygdryg/OneShot_Termux_installer/master/installer.sh
+
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install -y root-repo 
+pkg install -y git tsu python wpa-supplicant pixiewps iw
+
+git clone --depth 1 https://github.com/drygdryg/OneShot OneShot
+
+chmod +x OneShot/oneshot.py
+
+printf "###############################################\n#  All done! Now you can run OneShot with\n#   sudo python OneShot/oneshot.py -i wlan0 -K\n#\n#  To update, run\n#   (cd OneShot && git pull)\n###############################################\n"
